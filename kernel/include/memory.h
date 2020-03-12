11c75b6f658e39d5d97c545a61bcf00a9fb38033
Jamie <jamie@coffeeunderrun.dev>
2020-03-12 13:25:16 -0400

    Track page table entry counts.

--- kernel/include/memory.h
+++ kernel/include/memory.h
@@ -1,10 +1,16 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <status.h>
+#include <stdbool.h>
 #include <stdint.h>
 
 typedef uint64_t addr_t;
 
 void init_memory(void *mmap);
+
+status_t kmalloc(addr_t vaddr, bool write, bool user);
+
+status_t kfree(addr_t vaddr);
 
 #endif // MEMORY_H
