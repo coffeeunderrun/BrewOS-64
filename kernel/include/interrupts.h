e180aa60a6223726c23ed46de55f2b980c9b73cc
Jamie <jamie@coffeeunderrun.dev>
2020-03-15 11:05:25 -0400

    Cleaned up architecture headers a bit.

--- kernel/include/interrupts.h
+++ kernel/include/interrupts.h
@@ -1,11 +1,6 @@
 #ifndef INTERRUPTS_H
 #define INTERRUPTS_H
 
-#include <cpu.h>
-#include <status.h>
-
-typedef struct irq_registers irq_registers_t;
-
 void init_interrupts(void);
 
 #endif // INTERRUPTS_H
