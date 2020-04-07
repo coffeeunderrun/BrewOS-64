958410dca60ac2ae098f46a23659ab5a20016842
Jamie <jamie@coffeeunderrun.dev>
2020-04-07 12:26:35 -0400

    Kernel shouldn't rely on loader GDT.

--- kernel/arch/x86_64/entry.s
+++ kernel/arch/x86_64/entry.s
@@ -6,13 +6,46 @@ section .text
 
 global entry
 entry:
+    ; Don't rely on the stage 2 loader GDT
+    mov rax, qword gdt.ptr
+    lidt [rax]
+
+    mov rax, qword trampoline
+    jmp gdt.kern_code:rax
+
+trampoline:
+    mov ax, gdt.kern_data
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    ; Set up new stack
     mov rsp, qword kernel_stack_top
 
+    ; Initialize FPU
     call init_fpu
 
+    ; Jump to kernel
     jmp KernelMain
 
 section .bss
+
+align 0x1000
+global gdt.kern_code, gdt.user_code
+gdt:
+.null:       dq 0
+.kern_code:  equ $ - gdt
+             dq 0x00209A0000000000
+.kern_data:  equ $ - gdt
+             dq 0x0000920000000000
+.user_code:  equ $ - gdt
+             dq 0x0020FA0000000000
+.user_data:  equ $ - gdt
+             dq 0x0000F20000000000
+.ptr:        dw $ - gdt - 1
+             dq gdt
 
 align 0x1000
 resb 0x2000
