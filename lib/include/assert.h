efb0ff85333af2c015480dff4beb2ac6c64f4de1
Jamie <jamie@coffeeunderrun.dev>
2020-04-13 12:13:18 -0400

    Bare minimum to get libcstd++ to actually build.

---
+++ lib/include/assert.h
@@ -0,0 +1,14 @@
+#ifndef ASSERT_H
+#define ASSERT_H
+
+#define assert(expr) ((void) 0)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ASSERT_H
---
+++ lib/include/inttypes.h
@@ -0,0 +1,6 @@
+#ifndef INTTYPES_H
+#define INTTYPES_H
+
+#include <sys/types.h>
+
+#endif // INTTYPES_H
