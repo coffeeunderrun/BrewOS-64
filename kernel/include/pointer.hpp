4cfc4e4919335012ba6b5eab2f845ca0b9f48ade
Jamie <jamie@coffeeunderrun.dev>
2020-04-21 22:29:51 -0400

    Shared smart pointer with reference count.

---
+++ kernel/include/pointer.hpp
@@ -0,0 +1,146 @@
+#ifndef KERNEL_POINTER
+#define KERNEL_POINTER
+
+#include <cassert>
+#include <reference.hpp>
+
+namespace BrewOS {
+
+template <class T>
+class Pointer
+{
+private:
+    T *m_data;
+
+    Reference *m_ref;
+
+public:
+    Pointer(void) : m_data(nullptr), m_ref(new Reference())
+    {
+    }
+
+    Pointer(T *data) : m_data(data), m_ref(new Reference())
+    {
+        // Do not increase reference count if data is NULL
+        if(m_data == nullptr)
+        {
+            return;
+        }
+
+        // Increase reference count
+        (*m_ref)++;
+    }
+
+    Pointer(const Pointer<T> &ptr) : m_data(ptr.m_data), m_ref(ptr.m_ref)
+    {
+        assert(m_ref != nullptr);
+        
+        // Do not increase reference count if data is NULL
+        if(m_data == nullptr)
+        {
+            return;
+        }
+
+        // Increase reference count
+        (*m_ref)++;
+    }
+
+    ~Pointer(void)
+    {
+        assert(m_ref != nullptr);
+
+        // Do not delete data if there are still references
+        if(--(*m_ref) > 0)
+        {
+            return;
+        }
+
+        if(m_data != nullptr)
+        {
+            delete m_data;
+        }
+
+        delete m_ref;
+    }
+
+    Pointer &operator=(const Pointer ptr)
+    {
+        assert(m_ref != nullptr);
+
+        // Check if both pointers reference the same data
+        if(m_data == ptr.m_data)
+        {
+            return *this;
+        }
+
+        // Remove reference to original data
+        if(m_data != nullptr)
+        {
+            m_data = nullptr;
+            (*m_ref)--;
+        }
+
+        // Use reference counter from new pointer
+        m_ref = ptr.m_ref;
+
+        // Use data from new pointer if available
+        if(ptr.m_data != nullptr)
+        {
+            m_data = ptr.m_data;
+            (*m_ref)++;
+        }
+
+        return *this;
+    }
+
+    T &operator*(void)
+    {
+        return *m_data;
+    }
+
+    T &operator[](int idx)
+    {
+        return m_data[idx];
+    }
+
+    T *operator->(void)
+    {
+        return m_data;
+    }
+
+    T *Get(void)
+    {
+        return m_data;
+    }
+
+    void Reset(T *data = nullptr)
+    {
+        assert(m_ref != nullptr);
+
+        // Do not reset if original data is the same as the new data
+        if(m_data == data)
+        {
+            return;
+        }
+
+        // Remove reference to original data
+        if(m_data != nullptr)
+        {
+            (*m_ref)--;
+        }
+
+        // Create new reference counter if original is still being used
+        if(*m_ref > 0)
+        {
+            m_ref = new Reference();
+        }
+
+        // Reference new data
+        m_data = data;
+        (*m_ref)++;
+    }
+};
+
+} // BrewOS
+
+#endif // KERNEL_POINTER
---
+++ kernel/include/reference.hpp
@@ -0,0 +1,86 @@
+#ifndef KERNEL_REFERENCE
+#define KERNEL_REFERENCE
+
+#include <cassert>
+
+namespace BrewOS {
+
+class Reference
+{
+private:
+    int m_cnt;
+
+public:
+    Reference(int cnt = 0) : m_cnt(cnt)
+    {
+        assert(m_cnt >= 0);
+    }
+
+    ~Reference(void)
+    {
+        assert(m_cnt == 0);
+    }
+
+    Reference &operator=(const Reference &)
+    {
+        return *this;
+    }
+
+    bool operator==(const int val)
+    {
+        return m_cnt == val;
+    }
+
+    bool operator!=(const int val)
+    {
+        return m_cnt != val;
+    }
+
+    bool operator>(const int val)
+    {
+        return m_cnt > val;
+    }
+
+    bool operator>=(const int val)
+    {
+        return m_cnt >= val;
+    }
+
+    bool operator<(const int val)
+    {
+        return m_cnt < val;
+    }
+
+    bool operator<=(const int val)
+    {
+        return m_cnt <= val;
+    }
+
+    int &operator--(void)
+    {
+        assert(m_cnt > 0);
+        return --m_cnt;
+    }
+
+    int operator--(int)
+    {
+        assert(m_cnt > 0);
+        return m_cnt--;
+    }
+
+    int &operator++(void)
+    {
+        assert(m_cnt >= 0);
+        return ++m_cnt;
+    }
+
+    int operator++(int)
+    {
+        assert(m_cnt >= 0);
+        return m_cnt++;
+    }
+};
+
+} // BrewOS
+
+#endif // KERNEL_REFERENCE
